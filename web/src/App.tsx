87a618a27941696096e0437eb9cf7bfae3e213f5
Matthew Dobson <matthew.dobson1@outlook.com>
2025-08-27 19:06:27 +0100

    feat(web): changed strikezone to 5x5 incl miss-buttons, add basic colours, containerised strike zone better for future dev, split screen ready for temporary table to link button effects next

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,43 @@
-export default function StrikeZone() {
+export default function App() {
   return (
-    <div className="strike-zone-container">
-      <div className="strike-zone-row">
-        <button className="strike-zone-button">1</button>
-        <button className="strike-zone-button">2</button>
-        <button className="strike-zone-button">3</button>
-      </div>
-      <div className="strike-zone-row">
-        <button className="strike-zone-button">4</button>
-        <button className="strike-zone-button">5</button>
-        <button className="strike-zone-button">6</button>
-      </div>
-      <div className="strike-zone-row">
-        <button className="strike-zone-button">7</button>
-        <button className="strike-zone-button">8</button>
-        <button className="strike-zone-button">9</button>
-      </div>
+    <div className="page-layout">
+      <section className="left-pane">Table to come</section>
+
+      <section className="right-pane">
+        <div className="square-wrap">
+          <div className="grid-5x5">
+            <button className="miss-button">T1</button>
+            <button className="miss-button">T2</button>
+            <button className="miss-button">T3</button>
+            <button className="miss-button">T4</button>
+            <button className="miss-button">T5</button>
+
+            <button className="miss-button">L1</button>
+            <button className="strike-button">1</button>
+            <button className="strike-button">2</button>
+            <button className="strike-button">3</button>
+            <button className="miss-button">R1</button>
+
+            <button className="miss-button">L2</button>
+            <button className="strike-button">4</button>
+            <button className="strike-button">5</button>
+            <button className="strike-button">6</button>
+            <button className="miss-button">R2</button>
+
+            <button className="miss-button">L3</button>
+            <button className="strike-button">7</button>
+            <button className="strike-button">8</button>
+            <button className="strike-button">9</button>
+            <button className="miss-button">R3</button>
+
+            <button className="miss-button">B1</button>
+            <button className="miss-button">B2</button>
+            <button className="miss-button">B3</button>
+            <button className="miss-button">B4</button>
+            <button className="miss-button">B5</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
